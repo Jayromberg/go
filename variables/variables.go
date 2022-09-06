f2ab3bb4111c4d4348aeab41c8262cbde9747e0b
Jayromberg <jayro.dev@gmail.com>
2022-09-06 12:48:24 -0300

    feat: Tipos de variaveis no Go

---
+++ variables/variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    var name string = "Jayro"
+    var age int = 31
+    var version float32 = 1.1
+    fmt.Println("Olá sr.", name, "sua idade é", age)
+    fmt.Println("Este programa está na versão", version)
+}
