a29b069d28c27b26f69c0079aadcbb052b430151
Jayromberg <jayro.dev@gmail.com>
2022-09-22 09:37:58 -0300

    feat: Controle de fluxo

--- monitorador/monitorador.go
+++ monitorador/monitorador.go
@@ -3,17 +3,27 @@ package main
 import "fmt"
 
 func main() {
-    name := "Jayro"
-    version := 1.1
-    fmt.Println("Olá sr.", name)
-    fmt.Println("Este programa está na versão", version)
+	name := "Jayro"
+	version := 1.1
+	fmt.Println("Olá sr(a).", name)
+	fmt.Println("Este programa está na versão", version)
 
 	fmt.Println("1- Iniciar Monitoramento")
-    fmt.Println("2- Exibir Logs")
-    fmt.Println("0- Sair do Programa")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
 
-    var comando int
-    fmt.Scan(&comando)
+	var comando int
+	fmt.Scan(&comando)
 
-    fmt.Println("O comando escolhido foi", comando) 
+	fmt.Println("O comando escolhido foi", comando)
+
+	if comando == 1 {
+		fmt.Println("Monitorando...")
+	} else if comando == 2 {
+		fmt.Println("Exibindo Logs...")
+	} else if comando == 0 {
+		fmt.Println("Saindo do programa...")
+	} else {
+		fmt.Println("Não conheço este comando")
+	}
 }
--- variables/variables.go
+++ variables/variables.go
@@ -1,16 +1,18 @@
 package main
 
-import "fmt"
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-    name := "Jayro"
-    age := 31
-    version := 1.1
-    fmt.Println("Olá sr.", name, "sua idade é", age)
-    fmt.Println("Este programa está na versão", version)
+	name := "Jayro"
+	age := 31
+	version := 1.1
+	fmt.Println("Olá sr.", name, "sua idade é", age)
+	fmt.Println("Este programa está na versão", version)
 
-    fmt.Println("O tipo da variável name é", reflect.TypeOf(name))
-    fmt.Println("O tipo da variável age é", reflect.TypeOf(age))
-    fmt.Println("O tipo da variável version é", reflect.TypeOf(version))
+	fmt.Println("O tipo da variável name é", reflect.TypeOf(name))
+	fmt.Println("O tipo da variável age é", reflect.TypeOf(age))
+	fmt.Println("O tipo da variável version é", reflect.TypeOf(version))
 }
