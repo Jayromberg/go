36ca56a8fb80c4e45b167905edc2a7606cbc53db
Jayromberg <jayro.dev@gmail.com>
2022-09-22 10:07:35 -0300

    refactor: Introdução às funções

--- monitorador/monitorador.go
+++ monitorador/monitorador.go
@@ -1,21 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	name := "Jayro"
-	version := 1.1
-	fmt.Println("Olá sr(a).", name)
-	fmt.Println("Este programa está na versão", version)
 
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do Programa")
-
-	var comando int
-	fmt.Scan(&comando)
-
-	fmt.Println("O comando escolhido foi", comando)
+	exibeIntroducao()
+	exibeMenu()
+	comando := leComando()
 
 	switch comando {
 	case 1:
@@ -24,7 +18,30 @@ func main() {
 		fmt.Println("Exibindo Logs...")
 	case 0:
 		fmt.Println("Saindo do programa...")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
 	}
+}
+
+func exibeIntroducao() {
+	nome := "Jayro"
+	versao := 1.1
+	fmt.Println("Olá, sr(a).", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
 }
